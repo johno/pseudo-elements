bbdffb4bfcf48518c1a01a7f7276716248dad589
John Otander <johnotander@gmail.com>
2015-01-20 14:51:54 -0700

    Add test

--- test/test.js
+++ test/test.js
@@ -1,9 +1,11 @@
 var assert = require('assert');
 var pseudoElements = require('..');
 
+var elements = ['after', 'before', 'first-letter', 'first-line', 'selection'];
+
 describe('pseudo-elements', function() {
 
-  it('should do something awesome', function() {
-    assert.equal(pseudoElements(), true);
+  it('should do return a list of pseudo-elements', function() {
+    assert.deepEqual(pseudoElements(), elements);
   });
 });
